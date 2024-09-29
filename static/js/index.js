4561fa2eec39f45e11da64b66f8aa45ea85a29e5
Suvir Mirchandani <smirchan@gmail.com>
2024-09-29 16:31:33 -0700

    Video buttons and bibtex

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,7 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
 
@@ -19,21 +17,16 @@ $(document).ready(function() {
 			autoplaySpeed: 3000,
     }
 
-		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
-    // Loop on each carousel initialized
     for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
     	carousels[i].on('before:show', state => {
     		console.log(state);
     	});
     }
 
-    // Access to bulmaCarousel instance of an element
     var element = document.querySelector('#my-element');
     if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
     	element.bulmaCarousel.on('before-show', function(state) {
     		console.log(state);
     	});
@@ -43,3 +36,46 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+let currentTask = null;
+
+function changeTask(taskName, videoSrc) {
+  const videoPlayer = document.getElementById('task-video-player');
+  
+  if (videoPlayer) {
+    videoPlayer.src = videoSrc;
+    
+    document.querySelectorAll('.task-selector .button').forEach(button => {
+      button.classList.remove('is-active');
+    });
+    
+    const clickedButton = document.querySelector(`.task-selector .button[onclick*="${taskName}"]`);
+    if (clickedButton) {
+      clickedButton.classList.add('is-active');
+    }
+  }
+}
+
+function changeCrowdVideo(crowdName, videoSrc) {
+  const videoPlayer = document.getElementById('crowd-video-player');
+
+  if (videoPlayer) {
+    videoPlayer.src = videoSrc;
+    
+    document.querySelectorAll('.crowd-selector .button').forEach(button => {
+      button.classList.remove('is-active');
+    });
+    
+    const clickedButton = document.querySelector(`.crowd-selector .button[onclick*="${crowdName}"]`);
+    if (clickedButton) {
+      clickedButton.classList.add('is-active');
+    }
+  }
+
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  changeTask('hi-chew', 'static/videos/expert-hi-chew-web.mp4');
+  changeCrowdVideo('Example #1', 'static/videos/crowd-1-web.mp4');
+});
